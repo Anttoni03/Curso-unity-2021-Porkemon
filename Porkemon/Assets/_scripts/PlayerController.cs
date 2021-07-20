f40fd9da5ddec338805f50fbb3e49f86bef70e70
Anttoni03 <tonifg2003@gmail.com>
2021-07-20 18:51:09 +0200

    Cosa 2D

---
+++ Porkemon/Assets/_scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private bool isMoving = false;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 input;
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0)
+                input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+
+                StartCoroutine(MoveTowards(targetPosition));
+            }
+        }
+    }
+
+    IEnumerator MoveTowards(Vector3 destination)
+    {
+        isMoving = true;
+        while (Vector3.Distance(transform.position,destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = destination;
+        isMoving = false;
+    }
+}
